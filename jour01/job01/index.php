a03d593a98ee90c46d910a35b232b4846e5c3e69
unknown <ivann.machado@laplateforme.io>
2025-07-21 11:30:42 +0200

    job01

---
+++ jour01/job01/index.php
@@ -0,0 +1,23 @@
+<?php 
+$str = 'LaPlateforme';
+echo $str;
+echo '<br/>';
+$str2 = 'Vive';
+$str3 = '!';
+echo ' ' . $str2 . ' ' . $str . ' ' . $str3;
+echo '<br/>';
+$val = 6;
+echo $val;
+echo '<br/>';
+echo $val + 4;
+echo '<br/>';
+$myBool = true;
+echo $myBool;
+echo '<br/>';
+var_export($myBool);
+echo '<br/>';
+$myBool = !$myBool;
+echo $myBool;
+echo '<br/>';
+var_export($myBool);
+?>

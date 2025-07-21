7f64f58bd612d109897d8450440de1106780ccd2
unknown <ivann.machado@laplateforme.io>
2025-07-21 11:48:01 +0200

    case

--- jour01/job03/index.php
+++ jour01/job03/index.php
@@ -6,7 +6,7 @@ $str = 'Yeet';
 echo '<table border="1">';
 echo '<tr><th>Type</th><th>Name</th><th>Value</th></tr>';
 echo '<tr><td>Boolean</td><td>$bool</td><td>' . ($bool ? 'true' : 'false') . '</td></tr>';
-echo '<tr><td>integer</td><td>$int</td><td>' . $int . '</td></tr>';
+echo '<tr><td>Integer</td><td>$int</td><td>' . $int . '</td></tr>';
 echo '<tr><td>Float</td><td>$float</td><td>' . $float . '</td></tr>';
 echo '<tr><td>String</td><td>$str</td><td>' . $str . '</td></tr>';
 echo '</table>';

92f57d473847923ba536b9392f7fa80f18091912
unknown <ivann.machado@laplateforme.io>
2025-07-21 20:01:27 +0200

    job07 j2

--- jour02/job07/index.php
+++ jour02/job07/index.php
@@ -1,36 +1,15 @@
 <?php 
-$heigth = 10;
-$width = 20;
-echo '<div style="width: ' . $width*10 . 'px; height: ' . $heigth*10 . 'px; border: solid 1px; display: block;">';
-/*for ($x = 0; $x < $width; $x++) { 
-	for ($y = 0; $y < $heigth; $y++) { 
-		if ($x === 0 || $x === $width - 1 || $y === 0 || $y === $heigth - 1) {
-			if ($x === 0 && $y === 0) { 
-				echo '&boxdr;';
-			}
-			elseif ($x === 0 && $y === $heigth - 1) {
-				echo '&boxdl;';
-			}
-			elseif ($x === $width - 1 && $y === 0) {
-				echo '&boxur;';
-			}
-			elseif ($x === $width - 1 && $y === $heigth - 1) {
-				echo '&boxul;';
-			}
-			elseif ($x === 0) {
-				echo '&macr;';
-			}
-			elseif ($x === $width - 1) {
-				echo '&lowbar;';
-			}
-			else {
-				echo '&vert;';
-			}
-		}
-		else {
-			echo '-.';
-		}
+$height = 5;
+echo '<div style="text-align: center; min-width: 20px; margin: auto; display: block;">';
+for ($i = 0; $i < $height; $i++) { 
+	echo '/';
+	if ($i && $i < $height - 1) {
+		echo str_repeat(' . ', $i);
 	}
-	echo '<br>';
-}*/
+	else {
+		echo str_repeat('_.', $i);
+	}
+	echo '\\<br/>';
+}
+echo '</div>';
 ?>

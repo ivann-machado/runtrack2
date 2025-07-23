f54b2a775e3318f77835b060860c98b89ffc22c1
unknown <ivann.machado@laplateforme.io>
2025-07-23 12:30:31 +0200

    job5 indent

--- jour07/job05/index.php
+++ jour07/job05/index.php
@@ -1,12 +1,12 @@
 <?php 
 function occurences(string $str, string $char): int {
-    if (strlen($char) === 1) {
-        
-        return substr_count(strtolower($str), $char);
-    }
-    else {
-        return 0;
-    }
+	if (strlen($char) === 1) {
+		
+		return substr_count(strtolower($str), $char);
+	}
+	else {
+		return 0;
+	}
 }
 
 echo occurences('Bonjour', 'b');

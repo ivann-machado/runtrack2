725378415db35b26dab4bf4ca32acf0ccb2657df
unknown <ivann.machado@laplateforme.io>
2025-07-28 14:32:04 +0200

    job5

---
+++ jour10/job05/index.php
@@ -0,0 +1,34 @@
+<?php
+$db = mysqli_connect('localhost', 'root', '', 'jour09');
+$etudiant = mysqli_query($db, 'SELECT * FROM `etudiants` WHERE `naissance` > DATE_ADD(CURDATE(), INTERVAL -18 YEAR)');
+
+if (mysqli_num_rows($etudiant) > 0): ?>
+	<table border='1'>
+		<thead>
+			<th>Id</th>
+			<th>Prenom</th>
+			<th>Nom</th>
+			<th>Naissance</th>
+			<th>Sexe</th>
+			<th>Email</th>
+		</thead>
+		<tbody>
+			<?php while ($row = mysqli_fetch_assoc($etudiant)): ?>
+				<tr>
+					<td><?php echo $row['id'] ?></td>
+					<td><?php echo $row['prenom']; ?></td>
+					<td><?php echo $row['nom']; ?></td>
+					<td><?php echo $row['naissance']; ?></td>
+					<td><?php echo $row['sexe']; ?></td>
+					<td><?php echo $row['email']; ?></td>
+				</tr>
+			<?php endwhile; ?>
+		</tbody>
+	</table>
+<?php else: ?>
+
+	<p>0 resultat</p>
+
+<?php endif;
+mysqli_close($db);
+?>
